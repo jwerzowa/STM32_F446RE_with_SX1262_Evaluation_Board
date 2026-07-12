bd871036ae53926cbccde0b2e95aa66ef778dfcc
jwerzowa <52651471+jwerzowa@users.noreply.github.com>
2026-07-11 22:07:48 -0600

    write basic main.c function to be analyzed with logic analyzer

--- src/main.c
+++ src/main.c
@@ -1,29 +1,35 @@
 #include "../drivers/inc/rcc.h"
 #include "../drivers/inc/gpio.h"
 #include "../drivers/inc/systick.h"
-
-void delay(uint32_t count) {
-    while(count--);
-}
-
+#include "../drivers/inc/spi.h"
 
 int main(void) {
     RCC_Config(180000000);
     SysTick_Init(180000000);
 
-    GPIO_PinConfig led = {
-        .pin = 5,
-        .mode = GPIO_MODE_OUTPUT,
-        .output_type = GPIO_OUTPUT_PUSH_PULL,
-        .speed = GPIO_SPEED_LOW,
-        .pull = GPIO_PULL_NONE,
-        .af = 0
+
+    SPI_Config spi1_config = {
+        .port = GPIOA,
+        .sck_pin = 5,
+        .mosi_pin = 7,
+        .miso_pin = 6,
+        .nss_pin = 8,
+        .af = 5, // AF5 for SPI1
+        .baudrate_prescaler = 0b011, // fPCLK/16
+        .cpol = 0,
+        .cpha = 0,
+        .dff = 0, // 8-bit data frame
+        .lsbfirst = 0 // MSB first
     };
 
-    GPIO_Init(GPIOA, &led);
 
-    while (1) {
-        GPIO_Toggle(GPIOA, 5);
-        SysTick_delay(1000);
-    }
+    SPI_Init(SPI1, &(spi1_config));
+
+    uint8_t tx_data = 0xC0;
+    uint8_t rx_data = 0x00;
+
+    CS_Select(GPIOA, 8);
+    SPI_transmit(SPI1, &tx_data, 1);
+    SPI_receive(SPI1, &rx_data, 1);
+    CS_Deselect(GPIOA, 8);
 }
